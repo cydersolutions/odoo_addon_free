b2d0863aa7b0913323e6064c807a9c869e5ebea9
Your Name <youremail@example.com>
2023-03-15 15:23:33 +1100

    fixed manifest. Added icon

--- trip/__manifest__.py
+++ trip/__manifest__.py
@@ -1,20 +1,21 @@
 {
     'name': 'Cyder Travel Log',
-    'version': '15.0.1.1.0',
+    'version': '16.0.1.1.0',
     'summary': 'Extends fleet to add travel information',
-    'category': 'Productivity',
     'author': 'Cyder Solutions',
-    'website': 'https://www.cyder.com.au',
+    'category': 'Productivity',
     'description': """
 Extends fleet to add travel information
 """,
+    'website': 'https://www.cyder.com.au',
     'depends': ['fleet'],
     'data': [
         'views/trips.xml'
     ],
+    'images': ['static/description/icon.png'],
     'installable': True,
     'application': True,
     'auto_install': False,
     'license': 'LGPL-3',
-    'sequence': -90,
+    'sequence': -101,
 }
