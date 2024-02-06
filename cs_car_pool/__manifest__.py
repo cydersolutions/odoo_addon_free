d86aeec9ba9a905e14b310e1f7a23e0be31e5bf9
Michael Piko <michael@piko.com.au>
2024-02-06 11:23:41 +1100

    initial commit

---
+++ cs_car_pool/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Pool Vehicle Log',
+    'version': '1.0.6',
+    'category': 'Business',
+    'author': 'Cyder Solutions',
+    'website': 'https://www.cyder.com.au/',
+    'summary': 'Car Pool Log',
+    'description': """
+    Track pool vehicle usage
+    """,
+    'depends': ['mail', 'hr'],
+    'data': [
+        'security/cs_vehicle_log_groups.xml',
+        'security/ir.model.access.csv',
+        'data/cs_vehicle_log_sequence.xml',
+        'reports/report.xml',
+        'reports/vehicle_usage_report.xml',
+        'reports/vehicle_trip_report.xml',
+        'reports/vehicle_report.xml',
+        'views/top_menu.xml',
+        'views/cs_vehicle_log_view.xml',
+        'views/cs_vehicle_log_vehicles_view.xml',
+        'views/vehicle_history_lines_view.xml',
+        'views/hr_employee_view.xml',
+    ],
+    'demo': [],
+    #'images': ['static/description/banner.gif'],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
