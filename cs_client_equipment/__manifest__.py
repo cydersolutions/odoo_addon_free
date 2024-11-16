6c117437b25d06ac8b801fa0f39c6f215096e553
Michael Piko <michael@piko.com.au>
2024-11-17 10:32:28 +1100

    updates

--- cs_client_equipment/__manifest__.py
+++ cs_client_equipment/__manifest__.py
@@ -12,7 +12,7 @@
 
 {
     'name': 'Client Equipment (18.0)',
-    'version': '1.0.3',
+    'version': '1.0.4',
     'category': 'Productivity',
     'author': 'Cyder Solutions',
     'website': 'https://www.cyder.com.au',
